2bd0157aa08bfb0b95fe796a819e8ed57000c90d
Adam Garscadden <adam@Adams-MacBook-Air.local>
2023-03-08 09:29:54 +0000

    V1: Replace intro with placeholder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Plumbing!</p>
+        <h1 className="text-5xl font-bold text-gray-900 w-full m-auto mb-3 text-center">
+          🚽 Toilets
+        </h1>
+        <p className="mb-10 w-3/4 text-center m-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          nesciunt ex atque iste est, nam asperiores dignissimos iusto earum, id
+          sapiente sunt sequi ullam autem beatae! Nesciunt vero deleniti optio?
+        </p>
       </header>
     </div>
   );
